72dbb5a713608f1cdf27911412b8b103de8eecdf
x-Arte <zmyArte010828>
2023-12-06 10:28:44 +0000

    2023/12/6 Task4:coppelia sim

--- functions/transform_angle.m
+++ functions/transform_angle.m
@@ -3,4 +3,5 @@ function delta_angle = transform_angle(original_angle)
 delta_angle = original_angle;
 delta_angle(2) = -original_angle(2);
 delta_angle(3) = -original_angle(3);
+delta_angle(5) = -original_angle(5);
 end
